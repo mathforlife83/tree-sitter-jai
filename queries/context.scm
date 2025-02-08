073a0c64abecb9ff10b675cea601a0df72cec326
Marcel Mikołajko <celo1234567890@gmail.com>
2025-02-08 10:37:16 +0100

    Add context.scm

---
+++ queries/context.scm
@@ -0,0 +1,7 @@
+(procedure_declaration) @context
+(static_if_statement) @context
+(if_statement) @context
+(else_clause) @context
+(for_statement) @context
+(enum_declaration) @context
+(struct_declaration) @context
